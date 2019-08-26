04176d5b2ff092f5d77be36688108457b4837138
Matheus Claud <soueuounaoquemsabe@gmail.com>
2019-08-26 17:49:57 -0300

    dd

---
+++ LEDA/R01-01-Rot-SimpleSorting-Simultaneous-Bubble-environment/src/main/java/sorting/variationsOfBubblesort/SimultaneousBubblesort.java
@@ -0,0 +1,30 @@
+package sorting.variationsOfBubblesort;
+
+import sorting.AbstractSorting;
+
+/**
+ * This algorithm applies two bubblesorts simultaneously. In a same iteration, a
+ * bubblesort pushes the greatest elements to the right and another bubblesort
+ * pushes the smallest elements to the left. At the end of the first iteration,
+ * the smallest element is in the first position (index 0) and the greatest
+ * element is the last position (index N-1). The next iteration does the same
+ * from index 1 to index N-2. And so on. The execution continues until the array
+ * is completely ordered.
+ */
+public class SimultaneousBubblesort<T extends Comparable<T>> extends
+		AbstractSorting<T> {
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		
+		if (leftIndex+1 == rightIndex) {
+			
+			if (array[leftIndex].compareTo(array[rightIndex]) > 0) {
+				util.Util.swap(array, leftIndex, rightIndex);
+			}
+		}
+		
+		if (leftIndex != rightIndex) {
+			
+			
+		}
+	}
+}
