2147d410eabf70ecb84411c58633d483acf600cd
MatheusClaudi <matheus.claudino@ccc.ufcg.edu.br>
2019-10-09 22:20:06 -0300

    s

--- Projeto-PLP/projetoHaskell.hs
+++ Projeto-PLP/projetoHaskell.hs
@@ -16,6 +16,9 @@ type Notas = [Nota]
 data Disciplina = Disciplina NomeDisciplina Sala Professor Notas
                                                                    deriving (Show, Read)
 
+data Compromisso = Compromisso
+                                 deriving (Show, Read)
+
 getKey :: IO [Char]
 getKey = reverse <$> getKey' ""
   where getKey' chars = do
@@ -30,53 +33,105 @@ a2 = do
   system "clear"
   putStr("o2")
 
-a3 = do
-  system "clear"
-  putStr("o3")
 
 a4 = do
   system "clear"
   putStr("o4")
 
-changeMainScreen :: Integer -> IO()
-changeMainScreen 0 = do a1
-changeMainScreen 1 = do a2
-changeMainScreen 2 = do a3
-changeMainScreen 3 = do a4
+
+
+
+changeMainScreen :: [Disciplina] -> [Compromisso] -> Integer -> IO()
+changeMainScreen disciplinas compromissos cursor | cursor == 0 = do a1;
+                                                 | cursor == 1 = do a2;                
+                                                 | cursor == 2 = do configuracoesScreen disciplinas compromissos 0 
+                                                 | cursor == 3 = do a4
 
                  
 optionsMainScreen :: [String]
 optionsMainScreen = [" Disciplinas", " Compromissos", " Configuracoes", " Tutorial"]
 
-actionMainScreen :: [Integer] -> [Integer] -> Integer -> [Char] -> IO()
-actionMainScreen disciplinas compromissos cursor action | action == "\ESC[B" = mainScreen disciplinas compromissos ((cursor+1) `mod` 4)
-                                                        | action == "\ESC[A" && cursor /= 0 = mainScreen disciplinas compromissos (cursor-1)
-                                                        | action == "\ESC[A" && cursor == 0 = mainScreen disciplinas compromissos 3
-                                                        | action == "\ESC[C" = changeMainScreen cursor
-                                                        | action == "\ESC[D" = return ()
+optionsConfiguracoesScreen :: [String]
+optionsConfiguracoesScreen = [" Cadastrar disciplina", " Atualizar disciplina", " Remover disciplina", " Resetar sistema"]
 
-showMainScreen :: [String] -> Integer -> Integer -> IO()
-showMainScreen [] cursor contador = return ()
-showMainScreen (o:os) cursor contador = do
+doMainScreen :: [Disciplina] -> [Compromisso] -> Integer -> [Char] -> IO()
+doMainScreen disciplinas compromissos cursor action | action == "\ESC[B" = mainScreen disciplinas compromissos ((cursor+1) `mod` 4)
+                                                    | action == "\ESC[A" && cursor /= 0 = mainScreen disciplinas compromissos (cursor-1)
+                                                    | action == "\ESC[A" && cursor == 0 = mainScreen disciplinas compromissos 3
+                                                    | action == "\ESC[C" = changeMainScreen disciplinas compromissos cursor
+                                                    | action == "\ESC[D" = exitScreen disciplinas compromissos
+                                                    | otherwise = mainScreen disciplinas compromissos cursor
+
+showSimpleScreen :: [String] -> Integer -> Integer -> IO()
+showSimpleScreen [] cursor contador = return ()
+showSimpleScreen (o:os) cursor contador = do
    if contador == cursor
       then 
       putStrLn("->" ++ o)
    else
       putStrLn("  " ++ o)
-   showMainScreen os cursor (contador+1)
+   showSimpleScreen os cursor (contador+1)
 
 
-mainScreen :: [Integer] -> [Integer] -> Integer -> IO ()
+mainScreen :: [Disciplina] -> [Compromisso] -> Integer -> IO ()
 mainScreen disciplinas compromissos cursor = do
    
    system "clear"
-   showMainScreen optionsMainScreen cursor 0
+   showSimpleScreen optionsMainScreen cursor 0
    
    hSetBuffering stdin NoBuffering
    hSetEcho stdin False
-   key <- getKey
-   actionMainScreen disciplinas compromissos cursor key
+   action <- getKey
+   doMainScreen disciplinas compromissos cursor action
 
+
+endRun :: IO ()
+endRun = do
+   system "clear"
+   putStrLn("Obrigado por Utilizar")
+   pause <- getKey
+   system "clear"
+   putStr ""
+
+doExitScreen :: [Disciplina] -> [Compromisso] -> String -> IO ()
+doExitScreen disciplinas compromissos action | action == "s" = endRun
+                                             | otherwise = mainScreen disciplinas compromissos 0
+    
+exitScreen :: [Disciplina] -> [Compromisso] -> IO ()
+exitScreen disciplinas compromissos = do
+   system "clear"
+   putStrLn("Digite (s) para encerrar a execucao ou (Outra tecla) para voltar para o menu");
+   
+   hSetBuffering stdin NoBuffering
+   hSetEcho stdin False
+   action <- getKey
+   doExitScreen disciplinas compromissos action
+
+changeConfiguracoesScreen :: Integer -> IO()
+changeConfiguracoesScreen 0 = do a1
+changeConfiguracoesScreen 1 = do a2
+changeConfiguracoesScreen 2 = do a2
+changeConfiguracoesScreen 3 = do a4
+
+doConfiguracoesScreen :: [Disciplina] -> [Compromisso] -> Integer -> [Char] -> IO()
+doConfiguracoesScreen disciplinas compromissos cursor action | action == "\ESC[B" = configuracoesScreen disciplinas compromissos ((cursor+1) `mod` 4)
+                                                    | action == "\ESC[A" && cursor /= 0 = configuracoesScreen disciplinas compromissos (cursor-1)
+                                                    | action == "\ESC[A" && cursor == 0 = configuracoesScreen disciplinas compromissos 3
+                                                    | action == "\ESC[C" = changeConfiguracoesScreen cursor
+                                                    | action == "\ESC[D" = mainScreen disciplinas compromissos 0
+                                                    | otherwise = configuracoesScreen disciplinas compromissos cursor
+
+
+configuracoesScreen :: [Disciplina] -> [Compromisso] -> Integer -> IO ()
+configuracoesScreen disciplinas compromissos cursor = do
+   
+   system "clear"
+   showSimpleScreen optionsConfiguracoesScreen cursor 0
+   
+   hSetBuffering stdin NoBuffering
+   hSetEcho stdin False
+   action <- getKey
+   doConfiguracoesScreen disciplinas compromissos cursor action
 
 
 main :: IO ()
